deb3cb1ad18023c3f0221629b192ae893cba7166
Himanshu Sharma <himanshu.sharma9@tothenew.com>
2026-01-26 22:59:54 +0530

    Assignment1 Q8

---
+++ Self Study - Introduction to Java - 1/Q8.java
@@ -0,0 +1,21 @@
+public class Q8 {
+
+    public static void main(String[] args) {
+
+        System.out.println("----- ClassNotFoundException Demo -----");
+
+        try {
+            // This class does NOT exist
+            Class.forName("com.fake.UnknownClass");
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException occurred!");
+            System.out.println(e);
+        }
+
+        System.out.println("\n----- NoClassDefFoundError Demo -----");
+
+        // JVM will try to load Helper class
+        Helper h = new Helper();
+        h.show();
+    }
+}
