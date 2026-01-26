9125ad1c9ee9228ebd9ed89224daff624721959c
Himanshu Sharma <himanshu.sharma9@tothenew.com>
2026-01-26 22:46:15 +0530

    Assignment1 Q7

---
+++ Self Study - Introduction to Java - 1/Q7.java
@@ -0,0 +1,30 @@
+//Q7 WAP showing try, multi-catch and finally blocks.
+
+import java.util.Scanner;
+
+public class Q7 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter first number: ");
+            int a = sc.nextInt();
+
+            System.out.print("Enter second number: ");
+            int b = sc.nextInt();
+
+            int result = a / b;   // -> may cause ArithmeticException
+
+            System.out.println("Result = " + result);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Error: Invalid input or division by zero.");
+            System.out.println("Message: " + e.getMessage());
+        }
+        finally {
+            System.out.println("Finally block executed. Program ended safely.");
+            sc.close();
+        }
+    }
+}
