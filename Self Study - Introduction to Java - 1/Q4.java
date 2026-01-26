36375e8bfb5d3630ab3469394cb3d2006736bfde
Himanshu Sharma <himanshu.sharma9@tothenew.com>
2026-01-26 15:05:54 +0530

    Assignment1 Q4

---
+++ Self Study - Introduction to Java - 1/Q4.java
@@ -0,0 +1,49 @@
+// Q4.
+//
+// Create a two dimensional array of integers and display:
+//
+//        1. sum of all elements of each column
+//        2. sum of all elements of each row
+
+import java.util.Scanner;
+
+public class Q4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] arr = new int[rows][cols];
+
+        System.out.println("Enter elements of array:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("\nSum of each row:");
+        for (int i = 0; i < rows; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < cols; j++) {
+                rowSum += arr[i][j];
+            }
+            System.out.println("Row " + (i + 1) + " sum = " + rowSum);
+        }
+
+        System.out.println("\nSum of each column:");
+        for (int j = 0; j < cols; j++) {
+            int colSum = 0;
+            for (int i = 0; i < rows; i++) {
+                colSum += arr[i][j];
+            }
+            System.out.println("Column " + (j + 1) + " sum = " + colSum);
+        }
+        
+    }
+}
