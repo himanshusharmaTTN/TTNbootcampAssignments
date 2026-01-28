6bed35eb6da35d91e3cd85b4cc70445d77aa9fc6
himanshusharmaTTN <himanshu.sharma9@tothenew.com>
2026-01-28 22:46:09 +0530

    Update Q1.java

--- Collections/Q1.java
+++ Collections/Q1.java
@@ -1,3 +1,6 @@
+//  Q1. Write Java code to define List. 
+//  Insert 5 floating point numbers in List, and using an iterator, find the sum of the numbers in List.
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
