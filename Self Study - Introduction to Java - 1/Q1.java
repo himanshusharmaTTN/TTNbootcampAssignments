5cb319c2c2905afc0ece74273141e1e19c03aa86
himanshusharmaTTN <himanshu.sharma9@tothenew.com>
2026-01-28 15:42:03 +0530

    Update Q1.java

--- Self Study - Introduction to Java - 1/Q1.java
+++ Self Study - Introduction to Java - 1/Q1.java
@@ -2,17 +2,13 @@
 // Q1. Write a class with FirstName, LastName & age field. Print Firstname, LastName & age using static block, static method & static variable respectively.
 
 class Person {
-
-    // static variable
     static int age = 22;
-
-    // static block
+    
     static {
         String firstName = "Himanshu";
         System.out.println("First Name (from static block): " + firstName);
     }
 
-    // static method
     static void printLastName() {
         String lastName = "Sharma";
         System.out.println("Last Name (from static method): " + lastName);
