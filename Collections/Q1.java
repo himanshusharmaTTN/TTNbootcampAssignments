7ce810c3c2d74d405d5c3e38ead3dbd56900d94d
Himanshu Sharma <himanshu.sharma9@tothenew.com>
2026-01-28 22:42:35 +0530

    Collection Q1

---
+++ Collections/Q1.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+class ImplementingList
+{
+    static void main() {
+        List<Float> list = new ArrayList<>();
+
+        list.add(10.5f);
+        list.add(20.2f);
+        list.add(5.3f);
+        list.add(7.0f);
+        list.add(2.5f);
+
+        // Step 3: Use Iterator to find sum
+        Iterator<Float> it = list.iterator();
+        float sum = 0;
+        while(it.hasNext())
+        {
+            float x= it.next();
+            sum+=x;
+            System.out.println(x);
+        }
+        System.out.println("Sum is : "+sum);
+    }
+}
