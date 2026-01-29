5675f7aa8d74f82714d80732e11cce97043911a0
Himanshu Sharma <himanshu.sharma9@tothenew.com>
2026-01-29 22:23:25 +0530

    Q4

---
+++ Java8/Q4.java
@@ -0,0 +1,35 @@
+import java.util.function.Supplier;
+import java.util.function.BiFunction;
+
+class Student {
+
+    String name;
+    int age;
+
+    Student() {
+        System.out.println("No-arg constructor ");
+    }
+
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+        System.out.println("Parameterized constructor ");
+    }
+
+    public String toString() {
+        return "Student{name='" + name + "', age=" + age + "}";
+    }
+}
+
+ class ConstructorReference {
+
+    public static void main(String[] args) {
+
+        Supplier<Student> s2 = Student::new;
+        Student st2 = s2.get();
+
+        BiFunction<String, Integer, Student> f2 = Student::new;
+        Student st4 = f2.apply("Rahul", 24);
+        System.out.println(st4);
+    }
+}
